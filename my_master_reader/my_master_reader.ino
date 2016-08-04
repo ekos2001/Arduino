9b694afef61a9be11a462a6af84a11f9854f4d37
ekos2001 <ekos2001@gmail.com>
2016-08-04 00:10:57 -0400

    Added struct to send data, but Wire stopped working

--- my_master_reader/my_master_reader.ino
+++ my_master_reader/my_master_reader.ino
@@ -10,24 +10,28 @@
 #define WLAN_SSID       "linksys2"
 #define WLAN_PASS       "s0ftc0mputer"
 #define I2CAddressESPWifi 6
-const byte TIME_BETWEEN_READINGS = 10;            //Time between readings  
-const float Vcal_USA = 130.0;                             //Calibration for US AC-AC adapter 77DA-10-09
+
+typedef struct {
+  int power7, power6, Vrms;
+  unsigned long pulseCount;
+} PayloadTX;     // create structure - a neat way of packaging data for RF comms
+PayloadTX emontx;
+
+const byte TIME_BETWEEN_READINGS = 10;            //Time between readings
 ESP8266WebServer server(80);
 int blue = 2;
 int sda = 0;//GPIO0 is D3
 int scl = 4;//GPIO4 is D2
 WiFiClient client;                      // Create an ESP8266 WiFiClient class to connect to the MQTT server.
 char domain[] = "emoncms.org";
-float power6;
-float power7;
+//float power6;
+//float power7;
 String webResponse;
-Ticker ticker;
-
 
 void setup()
 {
   Serial.begin(9600);
-  Wire.begin(sda,scl);//Change to Wire.begin() for non ESP.
+  Wire.begin(sda, scl); //Change to Wire.begin() for non ESP.
   Serial.print(F("Connecting to "));
   Serial.println(WLAN_SSID);
   pinMode(blue, OUTPUT);
@@ -35,17 +39,17 @@ void setup()
   WiFi.begin(WLAN_SSID, WLAN_PASS);
   int counter = 0;
   while (WiFi.status() != WL_CONNECTED) {
-	  digitalWrite(blue, HIGH);
-	  delay(250);
-	  digitalWrite(blue, LOW);
-	  delay(250);
-	  Serial.print(F("."));
-	  counter++;
-	  if (counter > 50)
-	  {
-		  Serial.println("REBOOT");
-		  ESP.restart();
-	  }
+    digitalWrite(blue, HIGH);
+    delay(250);
+    digitalWrite(blue, LOW);
+    delay(250);
+    Serial.print(F("."));
+    counter++;
+    if (counter > 50)
+    {
+      Serial.println("REBOOT");
+      ESP.restart();
+    }
   }
   Serial.println();
 
@@ -55,137 +59,152 @@ void setup()
 
   setupOTA();
 
-  server.on("/", handle_root);
-  server.begin();
-//  ticker.attach(1.0, handleClient);
+  //  server.on("/", handle_root);
+  //  server.begin();
+  //  ticker.attach(1.0, handleClient);
+}
+
+void /*float*/ getPower()
+{
+  Wire.beginTransmission(I2CAddressESPWifi);
+  I2C_writeAnything(-1);
+  Wire.endTransmission();
+  delay(500);//Wait for Slave to calculate response.
+
+  Wire.requestFrom(I2CAddressESPWifi, /*4*/sizeof emontx);
+  Serial.print("Requested ");
+  //Serial.print(pin);
+  Serial.print(". Return:[");
+
+  //float irms;
+  I2C_readAnything(emontx);
+
+  //float power = irms*130.0;
+  //if (power < 30) {
+  //  power = 0;
+  //}
+
+  /*
+    Serial.print(irms);
+    Serial.print("A ");
+    Serial.print(power);
+    Serial.print("W ");
+  */
+  Serial.print("emontx.power7='");
+  Serial.print(emontx.power7);
+  Serial.print("' ");
+  Serial.print("emontx.power6='");
+  Serial.print(emontx.power6);
+  Serial.print("' ");
+  Serial.print("emontx.Vrms='");
+  Serial.print(emontx.Vrms);
+  Serial.print("' ");
+
+  Serial.println("]");
+
+  //  return power;
 }
 
 void loop()
 {
-	unsigned long start = millis();
-	if (WiFi.status() != WL_CONNECTED)
-	{
-		Serial.println("REBOOT");
-		ESP.restart();
-	}
+  unsigned long start = millis();
+  if (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.println("REBOOT");
+    ESP.restart();
+  }
 
-	ArduinoOTA.handle();
-	//server.handleClient();
+  ArduinoOTA.handle();
+  //server.handleClient();
 
-	power6 = getPower(6);
-	power7 = getPower(7);
+  getPower();
+  delay(300);
+  return;
+/*
+  power6 = getPower(6);
+  power7 = getPower(7);
 
 
 
-	Serial.println("connect to Server...");
-	if (client.connect(domain, 80))
-	{
-		digitalWrite(blue, LOW);
-		Serial.println("connected");
-		/*
-		client.print("GET /emoncms/input/post.json?apikey=a24ab9dffa26a22e1f6d50c289b11afa&node=20&csv=");
-		client.print(u);
-		client.print(",");
-		client.print(i);
-		*/
-		client.print("GET /emoncms/input/post.json?apikey=a24ab9dffa26a22e1f6d50c289b11afa&json={new_power6:");
-		client.print(power6);
-		client.print(",new_power7:");
-		client.print(power7);
-		client.print(",new_power6_7:");
-		client.print(power6 + power7);
-		client.print("}");
-		client.println();
-		char c;
-		long old = millis();
-		while( (millis()-old)<500 )    // 500ms timeout for 'ok' answer from server
-		{
-			while (client.available())
-			{
-				c = client.read();
-				Serial.write(c);
-			}
-		}
-		client.stop();
-		Serial.println("\nclosed");
-		digitalWrite(blue, HIGH);
-	}
-	else
-	{
-		Serial.println("Failed to connect");
-		digitalWrite(blue, HIGH);
-		delay(500);
-		digitalWrite(blue, LOW);
-		delay(500);
-		digitalWrite(blue, HIGH);
-		delay(500);
-		digitalWrite(blue, LOW);
-	}
-	unsigned long runtime = millis() - start;
-	unsigned long sleeptime = (TIME_BETWEEN_READINGS * 1000) - runtime - 100;
-	//delay(sleeptime);
-	delay(8000);
-}
-
-float getPower(int pin)
-{
-	Wire.beginTransmission(I2CAddressESPWifi);
-	I2C_writeAnything(pin);
-	Wire.endTransmission();
-	delay(500);//Wait for Slave to calculate response.
-
-	Wire.requestFrom(I2CAddressESPWifi, 4);
-	Serial.print("Requested ");
-	Serial.print(pin);
-	Serial.print(". Return:[");
-
-	float irms;
-	I2C_readAnything(irms);
-
-	float power = irms*130.0;
-	if (power < 30) {
-		power = 0;
-	}
-
-	Serial.print(irms);
-	Serial.print("A ");
-	Serial.print(power);
-	Serial.print("W ");
-	Serial.println("]");
-
-	return power;
+  Serial.println("connect to Server...");
+  if (client.connect(domain, 80))
+  {
+    digitalWrite(blue, LOW);
+    Serial.println("connected");
+//      client.print("GET /emoncms/input/post.json?apikey=a24ab9dffa26a22e1f6d50c289b11afa&node=20&csv=");
+//      client.print(u);
+//      client.print(",");
+//      client.print(i);
+    client.print("GET /emoncms/input/post.json?apikey=a24ab9dffa26a22e1f6d50c289b11afa&json={new_power6:");
+    client.print(power6);
+    client.print(",new_power7:");
+    client.print(power7);
+    client.print(",new_power6_7:");
+    client.print(power6 + power7);
+    client.print("}");
+    client.println();
+    char c;
+    long old = millis();
+    while ( (millis() - old) < 500 ) // 500ms timeout for 'ok' answer from server
+    {
+      while (client.available())
+      {
+        c = client.read();
+        Serial.write(c);
+      }
+    }
+    client.stop();
+    Serial.println("\nclosed");
+    digitalWrite(blue, HIGH);
+  }
+  else
+  {
+    Serial.println("Failed to connect");
+    digitalWrite(blue, HIGH);
+    delay(500);
+    digitalWrite(blue, LOW);
+    delay(500);
+    digitalWrite(blue, HIGH);
+    delay(500);
+    digitalWrite(blue, LOW);
+  }
+  unsigned long runtime = millis() - start;
+  unsigned long sleeptime = (TIME_BETWEEN_READINGS * 1000) - runtime - 100;
+  //delay(sleeptime);
+  delay(8000);
+  */
 }
 
 void setupOTA()
 {
-	// Port defaults to 8266
-	// ArduinoOTA.setPort(8266);
+  // Port defaults to 8266
+  // ArduinoOTA.setPort(8266);
 
-	// Hostname defaults to esp8266-[ChipID]
-	ArduinoOTA.setHostname("WeMos");
+  // Hostname defaults to esp8266-[ChipID]
+  ArduinoOTA.setHostname("WeMos");
 
-	// No authentication by default
-	// ArduinoOTA.setPassword((const char *)"123");
+  // No authentication by default
+  // ArduinoOTA.setPassword((const char *)"123");
 
-	ArduinoOTA.onStart([]() {
-		Serial.println("Start");
-	});
-	ArduinoOTA.onEnd([]() {
-		Serial.println("\nEnd");
-	});
-	ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
-		Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
-	});
-	ArduinoOTA.onError([](ota_error_t error) {
-		Serial.printf("Error[%u]: ", error);
-		if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
-		else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
-		else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
-		else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
-		else if (error == OTA_END_ERROR) Serial.println("End Failed");
-	});
-	ArduinoOTA.begin();
-	Serial.println("OTA Ready");
+  ArduinoOTA.onStart([]() {
+    Serial.println("Start");
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+  });
+  ArduinoOTA.begin();
+  Serial.println("OTA Ready");
 }
 
 void handleClient()
@@ -195,7 +214,7 @@ void handleClient()
 
 void handle_root()
 {
-  webResponse = String(power6) + " " + String(power7);
-	server.send(200, "text/plain", webResponse);
-	delay(100);
+//  webResponse = String(power6) + " " + String(power7);
+  server.send(200, "text/plain", webResponse);
+  delay(100);
 }
--- my_slave_sender/my_slave_sender.ino
+++ my_slave_sender/my_slave_sender.ino
@@ -3,27 +3,56 @@
 #include <Wire.h>
 
 #define I2CAddressESPWifi 6
-const int NUM_MONITORS = 8;
 
+typedef struct {
+  long power7, power6, Vrms;
+  unsigned long pulseCount;
+} PayloadTX;     // create structure - a neat way of packaging data for RF comms
+PayloadTX emontx;
+
+//const int NUM_MONITORS = 8;
 double irms = 0;
-EnergyMonitor monitors[NUM_MONITORS];
+//EnergyMonitor monitors[NUM_MONITORS];
+EnergyMonitor ct7, ct6;
+const float Ical7 =                59.3;              // (2000 turns / 22 Ohm burden) = 90.9; my is 32 Ohm, so 2000/32 = 62
+const float Ical6 =                59.3;
+const float Vcal =                 79.5;
+const int no_of_half_wavelengths = 30;
+const int timeout =                2000;                              //emonLib timeout
+const float phase_shift =          1.7;
+const byte pulse_count_pin =        3;
+const byte min_pulsewidth = 110;   // minimum width of interrupt pulse (default pulse output meters = 100ms)
+volatile byte pulseCount = 0;
+unsigned long pulsetime = 0;                                  // Record time of interrupt pulse
 
 void setup()
 {
   Serial.begin(9600);
+  pinMode(pulse_count_pin, INPUT_PULLUP);                     // Set emonTx V3.4 interrupt pulse counting pin as input (Dig 3 / INT1)
+  emontx.pulseCount = 0;                                      // Make sure pulse count starts at zero
 
-  for (int i = 0; i < NUM_MONITORS; i++)
-  {
-    monitors[i].current(i, 59.3);
-  }
-
-  for (int i = 0; i < NUM_MONITORS; i++)
-  {
-    for (int k = 0; k < 5; k++)
+  /*
+    for (int i = 0; i < NUM_MONITORS; i++)
     {
-      monitors[i].calcIrms(1480);
+      monitors[i].current(i, 59.3);
     }
-  }
+
+    for (int i = 0; i < NUM_MONITORS; i++)
+    {
+      for (int k = 0; k < 5; k++)
+      {
+        monitors[i].calcIrms(1480);
+      }
+    }
+  */
+
+  ct7.current(7, Ical7);             // CT ADC channel 7, calibration.
+  ct6.current(6, Ical6);             // CT ADC channel 6, calibration.
+
+  ct7.voltage(0, Vcal, phase_shift);          // ADC pin, Calibration, phase_shift
+  ct6.voltage(0, Vcal, phase_shift);          // ADC pin, Calibration, phase_shift
+
+  attachInterrupt(digitalPinToInterrupt(pulse_count_pin), onPulse, FALLING);     // Attach pulse counting interrupt pulse counting
 
   Wire.begin(I2CAddressESPWifi);
   Wire.onReceive(espWifiReceiveEvent);
@@ -33,32 +62,67 @@ void setup()
 void loop()
 {
   delay(1);
-	/*
-	Serial.print(monitors[6].calcIrms(1480));
-	Serial.print(" ");
-	Serial.println(monitors[7].calcIrms(1480));
-	*/
+  /*
+    Serial.print(monitors[6].calcIrms(1480));
+    Serial.print(" ");
+    Serial.println(monitors[7].calcIrms(1480));
+  */
 }
 
 void espWifiReceiveEvent(int count)
 {
+  unsigned long startTime = millis();
   long c;
   Serial.print("Received[");
   I2C_readAnything(c);
   Serial.print(c);
   Serial.println("]");
   //calc response.
-  if (c < 0)
+  /*
+    if (c < 0)
     irms  = monitors[0].readVcc();
-  else
+    else
     irms = monitors[c].calcIrms(1480);
-  Serial.print("IRMS='");
-  Serial.print(irms);
-  Serial.println("'");
+  */
+
+  ct7.calcVI(no_of_half_wavelengths, timeout); emontx.power7 = ct7.realPower;
+  ct6.calcVI(no_of_half_wavelengths, timeout); emontx.power6 = ct6.realPower;
+  emontx.Vrms = ct6.Vrms * 100;
+
+  if (pulseCount)                 // if the ISR has counted some pulses, update the total count
+  {
+    cli();                        // Disable interrupt just in case pulse comes in while we are updating the count
+    emontx.pulseCount += pulseCount;
+    pulseCount = 0;
+    sei();                        // Re-enable interrupts
+  }
+
+  Serial.print("emontx.power7='");
+  Serial.print(emontx.power7);
+  Serial.print("' ");
+  Serial.print("emontx.power6='");
+  Serial.print(emontx.power6);
+  Serial.print("' ");
+  Serial.print("emontx.Vrms='");
+  Serial.print(emontx.Vrms);
+  Serial.print("' ");
+  Serial.print("Done in '");
+  Serial.print(millis() - startTime);
+  Serial.print("' msec");
+  Serial.println();
 }
 
 void espWifiRequestEvent()
 {
-  I2C_singleWriteAnything(irms);
+  I2C_singleWriteAnything(emontx);
+}
+
+// The interrupt routine - runs each time a falling edge of a pulse is detected
+void onPulse()
+{
+  if ( (millis() - pulsetime) > min_pulsewidth) {
+    pulseCount++;          //calculate wh elapsed from time between pulses
+  }
+  pulsetime = millis();
 }
 
